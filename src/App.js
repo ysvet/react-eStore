9e588e203d097d38ce877ada69cac2d9a1b07d72
Yuri Svet <iurie.shvets@gmail.com>
2019-06-19 17:04:40 +0300

    navbar styled

--- src/App.js
+++ src/App.js
@@ -1,20 +1,29 @@
 import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
 import logo from './logo.svg';
 import './App.css';
+
+import Navbar from './components/Navbar';
+import ProductList from './components/ProductList';
+import Details from './components/Details';
+import Cart from './components/Cart';
+import Default from './components/Default';
+
 
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
-    <div className="row">
-    <div className="col-6">Column number 1</div>
-    <div className="col-6">
-      <span>
-        <i className="fas fa-home" />
-      </span>
-    </div>
-
-    </div>
+    <React.Fragment>
+      <Navbar />
+      <Switch>
+        <Route path="/" exact component={ProductList} />
+        <Route path="/details" component={Details} />
+        <Route path="/cart" component={Cart} />
+         <Route component={Default} />
+      </Switch>
+    </React.Fragment>
   );
 }
 
---
+++ src/components/ProductList.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+import Product from './Product';
+
+class ProductList extends Component {
+    render() {
+        return (
+            <div>
+               <Product /> 
+            </div>
+        );
+    }
+}
+
+export default ProductList;
