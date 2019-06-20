b8c70741266a7eb1500c818bc36ced4f1b25f156
Yuri Svet <iurie.shvets@gmail.com>
2019-06-20 13:12:46 +0300

    final commit

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -1,15 +1,33 @@
-import React, { Component } from 'react';
-
-import Product from './Product';
-
-class ProductList extends Component {
-    render() {
-        return (
-            <div>
-               <Product /> 
+import React, { Component } from "react";
+import Product from "./Product";
+import Title from "./Title";
+import { storeProducts } from "../data";
+import styled from "styled-components";
+import { ProductConsumer } from "../context";
+export default class ProductList extends Component {
+  state = {
+    products: storeProducts
+  };
+  render() {
+    return (
+      <React.Fragment>
+        <ProductWrapper className="py-5">
+          <div className="container">
+            <Title name="our" title="products" />
+            <div className="row">
+              <ProductConsumer>
+                {value => {
+                  return value.products.map(product => {
+                    return <Product key={product.id} product={product} />;
+                  });
+                }}
+              </ProductConsumer>
             </div>
-        );
-    }
+          </div>
+        </ProductWrapper>
+      </React.Fragment>
+    );
+  }
 }
 
-export default ProductList;
+const ProductWrapper = styled.section``;
